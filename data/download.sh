7083456bca8ff2e445d6b777ffbcb2b19178e4fa
Yong-Yeol Ahn <yyahn@indiana.edu>
2013-07-01 22:44:56 -0400

    data download script

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+wget http://yongyeol.com/data/newstapa-nis-original-tweets.csv
+wget http://yongyeol.com/data/newstapa-nis-rt-politics-tweets.csv
+wget http://yongyeol.com/data/newstapa-nis-rt-tweets.csv
